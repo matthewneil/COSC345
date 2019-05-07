258fc38616fa10c09460809292ba7347bca951ae
matthewneil <49290261+matthewneil@users.noreply.github.com>
2019-05-08 11:38:44 +1200

    Update main.c

--- main.c
+++ main.c
@@ -21,7 +21,6 @@ int main(int argc, char **argv)
     // Create a renderer (accelerated and in sync with the display refresh rate)
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     TTF_Init();
-    TTF_Font *font;
 
     float x = 390;
     float y = 50;
@@ -45,196 +44,274 @@ int main(int argc, char **argv)
     float ob5x = (rand() % 350)+200;
     float ob6x = (rand() % 300)+200;
 
+    SDL_Rect MainMenu_rect;
+    MainMenu_rect.x = 200;
+    MainMenu_rect.y = 50;
+    MainMenu_rect.w = 300;
+    MainMenu_rect.h = 50;
+
     bool running = true;
     bool move_left = false;
     bool move_right = false;
     bool move_up = false;
     bool move_down = false;
     SDL_Event event;
+    bool main_menu = true;
+
+    TTF_Font *font = NULL;
+    SDL_Color textColor = { 255, 0, 255 };
+    //Open the font
+    font = TTF_OpenFont( "/Windows/Fonts/Arial.ttf", 28 );
+
     while(running){
 
-        // Process events
-        while(SDL_PollEvent(&event)){
+        if (main_menu == true){
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            oby = 810;
+            speed = 3;
+            points = 0;
+            counted1 = 0;
+            counted2 = 0;
+            ob2y = 1110;
+            x = 390;
+            y = 50;
+            move_right = false;
+            move_left = false;
+            move_up = false;
+            move_down = false;
+            while(SDL_PollEvent(&event)){
+                if(event.type == SDL_QUIT){
+                    running = false;
+                }
+                if(event.key.keysym.sym == SDLK_SPACE){
+                    main_menu = false;
+                }
+            }
+            SDL_Color textColor = { 255, 255, 255 };
+            SDL_RenderClear(renderer);
+            SDL_Surface* mainMessage = TTF_RenderText_Solid(font, "Press space bar to start!", textColor);
+            SDL_Texture* mainMessage2 = SDL_CreateTextureFromSurface(renderer, mainMessage);
+            SDL_RenderCopy(renderer, mainMessage2, NULL, &MainMenu_rect);
+            SDL_RenderPresent(renderer);
+        } else {
+
             if(event.type == SDL_QUIT){
                 running = false;
             }
-            if(event.type == SDL_KEYUP){
-                if(event.key.keysym.sym == SDLK_RIGHT){
-                    move_right = false;
+            while(SDL_PollEvent(&event)){
+                if(event.type == SDL_KEYUP){
+                    if(event.key.keysym.sym == SDLK_RIGHT){
+                        move_right = false;
+                    }
+                    if(event.key.keysym.sym == SDLK_LEFT){
+                        move_left = false;
+                    }
+                    if(event.key.keysym.sym == SDLK_DOWN){
+                        move_down = false;
+                    }
+                    if(event.key.keysym.sym == SDLK_UP){
+                        move_up = false;
+                    }
                 }
-                if(event.key.keysym.sym == SDLK_LEFT){
-                    move_left = false;
-                }
-                if(event.key.keysym.sym == SDLK_DOWN){
-                    move_down = false;
-                }
-                if(event.key.keysym.sym == SDLK_UP){
-                    move_up = false;
-                }
-            }
-            if(event.type == SDL_KEYDOWN){
-                if(event.key.keysym.sym == SDLK_RIGHT){
-                    move_right = true;
-                }
-                if(event.key.keysym.sym == SDLK_LEFT){
-                    move_left = true;
-                }
-                if(event.key.keysym.sym == SDLK_DOWN){
-                    move_down = true;
-                }
-                if(event.key.keysym.sym == SDLK_UP){
-                    move_up = true;
+                if(event.type == SDL_KEYDOWN){
+                    if(event.key.keysym.sym == SDLK_RIGHT){
+                        move_right = true;
+                    }
+                    if(event.key.keysym.sym == SDLK_LEFT){
+                        move_left = true;
+                    }
+                    if(event.key.keysym.sym == SDLK_DOWN){
+                        move_down = true;
+                    }
+                    if(event.key.keysym.sym == SDLK_UP){
+                        move_up = true;
+                    }
                 }
             }
-        }
-        if(move_left){
-            x -= 5;
-        }
-        if(move_right){
-            x += 5;
-        }
-        if(move_up){
-            y -= 5;
-        }
-        if(move_down){
-            y += 5;
-        }
-
-        if(x < wallWidth || x > width - wallWidth - 20){
-            break;
-        }
-
-        oby -= speed;
-        ob2y -= speed;
-        if((oby <= y-5 && counted1 == 0)){
-            counted1 = 1;
-            points++;
-            printf("%d\n", points);
-            if(points % 10 == 0){
-                speed++;
+            if(move_left){
+                x -= 5;
             }
-        }
-        if((ob2y <= y-5 && counted2 == 0)){
-            counted2 = 1;
-            points++;
-            printf("%d\n", points);
-            if(points % 10 == 0){
-                speed++;
+            if(move_right){
+                x += 5;
             }
-        }
-        char array[64];
-        sprintf(array, "%d", points);
-        printf("%s\n", array);
-        //ob2y -= 0.1;
-        //ob3y -= 0.1;
-        if(oby < -50){
-            obx = (rand() % 320)+200;
-            ob2x = (rand() % 350)+200;
-            ob3x = (rand() % 300)+200;
-            oby = 600;
-            counted1 = 0;
-        }
-        if(ob2y < -50){
-            ob4x = (rand() % 320)+200;
-            ob5x = (rand() % 350)+200;
-            ob6x = (rand() % 300)+200;
-            ob2y = 600;
-            counted2 = 0;
-        }
-        /*
-        if(ob2y < -50){
-            ob2y = 600;
-        }*/
-        if(y < 0){
-            y = 0;
-        }
-        if(y > 550){
-            y = 550;
-        }
-
-        if(x + 20 >= obx && x <= obx + 70){
-            if(oby + 50 >= y && oby <= y+50){
-               break;
+            if(move_up){
+                y -= 5;
             }
-        }
-        if(x + 20 >= ob2x && x <= ob2x + 50){
-            if(oby + 50 >= y && oby <= y+50){
-               break;
+            if(move_down){
+                y += 5;
             }
-        }
 
-        if(x + 20 >= ob3x && x <= ob3x + 100){
-            if(oby + 50 >= y && oby <= y+50){
-               break;
+            if(x < wallWidth || x > width - wallWidth - 20){
+                break;
             }
-        }
 
-        if(x + 20 >= ob4x && x <= ob4x + 70){
-            if(ob2y + 50 >= y && ob2y <= y+50){
-               break;
+            oby -= speed;
+            ob2y -= speed;
+            if((oby <= y-5 && counted1 == 0)){
+                counted1 = 1;
+                points++;
+                printf("%d\n", points);
+                if(points % 10 == 0){
+                    speed++;
+                }
             }
-        }
-        if(x + 20 >= ob5x && x <= ob5x + 50){
-            if(ob2y + 50 >= y && ob2y <= y+50){
-               break;
+            if((ob2y <= y-5 && counted2 == 0)){
+                counted2 = 1;
+                points++;
+                printf("%d\n", points);
+                if(points % 10 == 0){
+                    speed++;
+                }
             }
-        }
 
-        if(x + 20 >= ob6x && x <= ob6x + 100){
-            if(ob2y + 50 >= y && ob2y <= y+50){
-               break;
+            if(!font){
+                return false;
             }
+
+            SDL_Rect Message_rect; //create a rect
+            Message_rect.x = 300;  //controls the rect's x coordinate
+            Message_rect.y = 0; // controls the rect's y coordinte
+            Message_rect.w = 20; // controls the width of the rect
+            Message_rect.h = 20;
+
+            SDL_Rect Message_rect2; //create a rect
+            Message_rect2.x = 250;  //controls the rect's x coordinate
+            Message_rect2.y = 0; // controls the rect's y coordinte
+            Message_rect2.w = 50; // controls the width of the rect
+            Message_rect2.h = 20;
+
+
+
+            //ob2y -= 0.1;
+            //ob3y -= 0.1;
+            if(oby < -50){
+                obx = (rand() % 320)+200;
+                ob2x = (rand() % 350)+200;
+                ob3x = (rand() % 300)+200;
+                oby = 600;
+                counted1 = 0;
+            }
+            if(ob2y < -50){
+                ob4x = (rand() % 320)+200;
+                ob5x = (rand() % 350)+200;
+                ob6x = (rand() % 300)+200;
+                ob2y = 600;
+                counted2 = 0;
+            }
+            /*
+            if(ob2y < -50){
+                ob2y = 600;
+            }*/
+            if(y < 0){
+                y = 0;
+            }
+            if(y > 550){
+                y = 550;
+            }
+
+            if(x + 20 >= obx && x <= obx + 70){
+                if(oby + 50 >= y && oby <= y+50){
+                   main_menu = true;
+                }
+            }
+            if(x + 20 >= ob2x && x <= ob2x + 50){
+                if(oby + 50 >= y && oby <= y+50){
+                   main_menu = true;
+                }
+            }
+
+            if(x + 20 >= ob3x && x <= ob3x + 100){
+                if(oby + 50 >= y && oby <= y+50){
+                   main_menu = true;
+                }
+            }
+
+            if(x + 20 >= ob4x && x <= ob4x + 70){
+                if(ob2y + 50 >= y && ob2y <= y+50){
+                   main_menu = true;
+                }
+            }
+            if(x + 20 >= ob5x && x <= ob5x + 50){
+                if(ob2y + 50 >= y && ob2y <= y+50){
+                   main_menu = true;
+                }
+            }
+
+            if(x + 20 >= ob6x && x <= ob6x + 100){
+                if(ob2y + 50 >= y && ob2y <= y+50){
+                   main_menu = true;
+                }
+            }
+
+
+            // Clear screen with black
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+
+            // Draw
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White color
+
+            SDL_Rect wall1 = {0, 0, 200, 600};
+            SDL_RenderFillRect(renderer, &wall1);
+
+            SDL_Rect wall2 = {600, 0, 200, 600};
+            SDL_RenderFillRect(renderer, &wall2);
+
+            SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); // Yellow color
+
+            // Draw a rectangle
+            SDL_Rect rect = {x, y, 20, 50};
+            SDL_RenderFillRect(renderer, &rect);
+
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_Rect rect2 = {obx, oby, 70, 50};
+            SDL_RenderFillRect(renderer, &rect2);
+
+            SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+            SDL_Rect rect3 = {ob2x, oby, 50, 50};
+            SDL_RenderFillRect(renderer, &rect3);
+
+            SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+            SDL_Rect rect4 = {ob3x, oby, 100, 50};
+            SDL_RenderFillRect(renderer, &rect4);
+
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+            SDL_Rect rect5 = {ob4x, ob2y, 70, 50};
+            SDL_RenderFillRect(renderer, &rect5);
+
+            SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
+            SDL_Rect rect6 = {ob5x, ob2y, 50, 50};
+            SDL_RenderFillRect(renderer, &rect6);
+
+            SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+            SDL_Rect rect7 = {ob6x, ob2y, 100, 50};
+            SDL_RenderFillRect(renderer, &rect7);
+
+            char array[64];
+            sprintf(array, "%d", points);
+            printf("%s\n", array);
+
+            SDL_Surface* surfaceMessage = TTF_RenderText_Solid(font, array, textColor); // as TTF_RenderText_Solid could only be used on SDL_Surface then you have to create the surface first
+            SDL_Surface* surfaceMessage2 = TTF_RenderText_Solid(font, "Score: ", textColor);
+
+            SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+            SDL_Texture* Message2 = SDL_CreateTextureFromSurface(renderer, surfaceMessage2);
+
+            SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+            SDL_RenderCopy(renderer, Message2, NULL, &Message_rect2);
+            // Show what was drawn
+
+            // Show what was drawn
+            SDL_RenderPresent(renderer);
         }
-
-
-        // Clear screen with black
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        // Draw
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White color
-
-        SDL_Rect wall1 = {0, 0, 200, 600};
-        SDL_RenderFillRect(renderer, &wall1);
-
-        SDL_Rect wall2 = {600, 0, 200, 600};
-        SDL_RenderFillRect(renderer, &wall2);
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255); // Yellow color
-
-        // Draw a rectangle
-        SDL_Rect rect = {x, y, 20, 50};
-        SDL_RenderFillRect(renderer, &rect);
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_Rect rect2 = {obx, oby, 70, 50};
-        SDL_RenderFillRect(renderer, &rect2);
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-        SDL_Rect rect3 = {ob2x, oby, 50, 50};
-        SDL_RenderFillRect(renderer, &rect3);
-
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-        SDL_Rect rect4 = {ob3x, oby, 100, 50};
-        SDL_RenderFillRect(renderer, &rect4);
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_Rect rect5 = {ob4x, ob2y, 70, 50};
-        SDL_RenderFillRect(renderer, &rect5);
-
-        SDL_SetRenderDrawColor(renderer, 255, 0, 255, 255);
-        SDL_Rect rect6 = {ob5x, ob2y, 50, 50};
-        SDL_RenderFillRect(renderer, &rect6);
-
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-        SDL_Rect rect7 = {ob6x, ob2y, 100, 50};
-        SDL_RenderFillRect(renderer, &rect7);
-
-        // Show what was drawn
-        SDL_RenderPresent(renderer);
 
 
     }
+
+    TTF_CloseFont(font);
+
+    TTF_Quit();
 
     // Release resources
     SDL_DestroyRenderer(renderer);
